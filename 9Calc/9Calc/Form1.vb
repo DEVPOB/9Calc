c3b81f65583269675b2eb2ad951640528c1b0bdf
DEVPOB <86759611+DEVPOB@users.noreply.github.com>
2022-02-13 11:41:58 +0700

    das
    das

--- 9Calc/9Calc/Form1.vb
+++ 9Calc/9Calc/Form1.vb
@@ -100,9 +100,6 @@ Public Class Form1
         ElseIf Sum = "/" Then
             TextBox1.Text = num1 / num2
         End If
-        If TextBox2.Text = "Github" Then
-            TextBox2.Text = "https://github.com/DEVPOB/9Calc"
-        End If
     End Sub
 
     Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
